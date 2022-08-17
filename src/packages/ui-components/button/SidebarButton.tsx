6a0076ca7a3f88e81aa73f43895dad32f6415fa9
Moses Chukwunekwu <dantereus1@gmail.com>
2022-08-17 20:23:10 +0100

    cleanup

--- src/packages/ui-components/button/SidebarButton.tsx
+++ src/packages/ui-components/button/SidebarButton.tsx
@@ -2,7 +2,7 @@ import { SidebarButtonRoot } from "./Button.styles";
 import { IconButton } from "./IconButton";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
-export const SidebarButton: React.FunctionComponent = ({}) => {
+export const SidebarButton: React.FunctionComponent = () => {
   return (
     <SidebarButtonRoot>
       <IconButton icon={<AddRoundedIcon />} variant="contained" />
